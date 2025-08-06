1cc292ad1f1ff63fe8deef7b0b41dad010f8e736
Eduardo Meneses <eduardomoraesdeveloper@gmail.com>
2025-08-05 22:14:01 -0300

    fix: update API_URL to use process.env.API_URL; refactor Vite configuration to load environment variables correctly

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,26 @@
-import path from "path"
-import tailwindcss from "@tailwindcss/vite"
-import react from "@vitejs/plugin-react"
-import { defineConfig } from "vite"
+import path from "path";
+import tailwindcss from "@tailwindcss/vite";
+import react from "@vitejs/plugin-react";
+import { defineConfig, loadEnv } from "vite";
 
-export default defineConfig({
-  plugins: [react(), tailwindcss()],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd());
+
+  const processEnv = Object.keys(env)
+    .filter((key) => key.startsWith("VITE_"))
+    .reduce((acc, key) => {
+      const newKey = key.replace(/^VITE_/, "");
+      acc[`process.env.${newKey}`] = JSON.stringify(env[key]);
+      return acc;
+    }, {} as Record<string, string>);
+
+  return {
+    plugins: [react(), tailwindcss()],
+    resolve: {
+      alias: {
+        "@": path.resolve(__dirname, "./src"),
+      },
     },
-  },
-})
+    define: processEnv,
+  };
+});
